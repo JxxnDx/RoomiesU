87d7b6e2c7ab6229055d5f6556cdbf63dce86fd4
Juan Pallares <juan2220079@correo.uis.edu.co>
2025-03-07 17:00:11 -0500

    Eliminando espacios en authService

--- backend/src/services/authService.js
+++ backend/src/services/authService.js
@@ -9,14 +9,11 @@ export class AuthService {
             if (existingUser) {
                 return { error: "El correo ya está en uso." };
             }
-
             // Generar salt y hash de la contraseña
             const salt = crypto.randomBytes(16).toString("hex");
             const hash = crypto.createHmac("sha256", salt).update(userData.password).digest("hex");
-
             // Insertar usuario en la base de datos
             const userId = await Auth.create({ ...userData, salt, hash }, tableName);
-
             return { id: userId };
         } catch (error) {
             console.error(" Error en el registro:", error);
@@ -28,18 +25,14 @@ export class AuthService {
         try {
             // Buscar usuario
             const user = await Auth.findByEmail(correo, tableName);
-
             if (!user) {
                 return { error: "Credenciales inválidas" };
             }
-
             // Verificar la contraseña con SHA-256 y el salt
             const hashedPassword = crypto.createHmac("sha256", user.Salt).update(password).digest("hex");
-
             if (hashedPassword !== user.Hash) {
                 return { error: "Credenciales inválidas" };
             }
-
             return { message: "Inicio de sesión exitoso", userId: user.ID };
         } catch (error) {
             console.error(" Error en el login:", error);
