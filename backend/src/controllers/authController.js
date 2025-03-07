9f96dc07cef1f9cd4123c1bf8da378028e122361
Juan Pallares <juan2220079@correo.uis.edu.co>
2025-03-07 16:11:37 -0500

    Eliminando carpeta assets

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -3,11 +3,9 @@ import { AuthService } from "../services/authService.js";
 export const registerAdmin = async (req, res) => {
     try {
         const result = await AuthService.register(req.body, "administrador");
-
         if (result.error) {
             return res.status(400).json({ message: result.error });
         }
-
         res.status(201).json({ message: "Admin registrado con éxito", userId: result.id });
     } catch (error) {
         res.status(500).json({ message: "Error interno del servidor" });
@@ -17,11 +15,9 @@ export const registerAdmin = async (req, res) => {
 export const registerStudent = async (req, res) => {
     try {
         const result = await AuthService.register(req.body, "estudiante");
-
         if (result.error) {
             return res.status(400).json({ message: result.error });
         }
-
         res.status(201).json({ message: "Estudiante registrado con éxito", userId: result.id });
     } catch (error) {
         res.status(500).json({ message: "Error interno del servidor" });
@@ -31,11 +27,9 @@ export const registerStudent = async (req, res) => {
 export const loginAdmin = async (req, res) => {
     try {
         const result = await AuthService.login(req.body, "administrador");
-
         if (result.error) {
             return res.status(401).json({ message: result.error });
         }
-
         res.status(200).json(result);
     } catch (error) {
         res.status(500).json({ message: "Error interno del servidor" });
@@ -45,11 +39,9 @@ export const loginAdmin = async (req, res) => {
 export const loginStudent = async (req, res) => {
     try {
         const result = await AuthService.login(req.body, "estudiante");
-
         if (result.error) {
             return res.status(401).json({ message: result.error });
         }
-
         res.status(200).json(result);
     } catch (error) {
         res.status(500).json({ message: "Error interno del servidor" });
