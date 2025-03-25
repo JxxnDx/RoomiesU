f77abc4f42601c5437932517e2b976055ac710ef
Juan Pallares <juan2220079@correo.uis.edu.co>
2025-03-24 20:23:36 -0500

    Primera versión home

---
+++ frontend/src/components/ventajas.jsx
@@ -0,0 +1,9 @@
+export default function Ventaja ({titulo, img, children}) {
+    return (
+        <div className="flex flex-col items-center">
+            <img src={img} alt="Icono de seguridad" className="w-24 h-24" />
+            <h3 className="text-2xl font-bold">{titulo}</h3>
+            <p className="text-gray-700 text-center">{children}</p>
+          </div>
+    )
+}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,30 +1,12 @@
 import React from 'react'
 import { Link } from "react-router-dom"
+import Ventaja from '../components/ventajas'
 
 export default function Home() {
   return (
-    <div className="font-montserrat text-gray-900">
-      <header className="flex justify-between items-center px-6 py-4 bg-gray-800 text-white fixed w-full top-0 shadow-md">
+    <div className="font-montserrat text-gray-900 m-0 p-0">
+      <header className="flex flex-row justify-between bg-gray-900 text-white top-0 left-0 text-justify items-center p-4 w-full">
         <h1 className="text-4xl font-bold">RoomiesU</h1>
-        <nav>
-          <ul className="flex gap-8 text-green-600 font-bold">
-            <li>
-              <Link to="#" className="links-header">
-                Sobre Nosotros
-              </Link>
-            </li>
-            <li>
-              <Link to="#" className="links-header">
-                Habitaciones
-              </Link>
-            </li>
-            <li>
-              <Link to="#" className="links-header">
-                Experiencias
-              </Link>
-            </li>
-          </ul>
-        </nav>
         <nav>
           <ul className="flex gap-2 text-green-600 font-bold">
             <li>
@@ -41,12 +23,36 @@ export default function Home() {
         </nav>
       </header>
 
-      {/* <section className="text-center p-8 flex flex-col items-center justify-center mt-20">
-        <h1 className="text-3xl font-bold">Bienvenido/a!</h1>
-        <p className="text-lg text-gray-700">
-          Encuentra la habitación perfecta para ti en RoomiesU.
+      <article className="text-5xl mx-auto my-0 text-center w-[60%] pt-20">
+        <h1 className="text-7xl font-bold">Bienvenido/a!</h1>
+        <p className="text-gray-700 m-8">
+          Encuentra la habitación perfecta para ti en RoomiesU. <br />
+          Un segundo hogar para tu vida universitaria.
         </p>
-      </section> */}
+      </article> 
+
+      <section className='p-10 border-2 border-green-600 rounded-2xl w-[60%] mx-auto my-20'> 
+        <h2 className="text-4xl font-bold text-center pb-8">¿Por qué RoomiesU?</h2>
+        <div className="grid grid-cols-2 grid-rows-2 gap-8">
+          <Ventaja titulo="Seguridad" img="../seguridad.png">
+            Encuentra la habitación perfecta para ti. <br /> Todas nuestras habitaciones están amuebladas.
+          </Ventaja>
+          <Ventaja titulo="Comodidad" img="../comodidad.png">
+            Encuentra la habitación perfecta para ti. <br /> Puedes buscar una habitación según tus necesidades.
+          </Ventaja>
+          <Ventaja titulo="Al momento" img="../clic.png">
+            A un clic de postularte a una pensión. <br /> A un clic de publicar tu pensión.
+          </Ventaja>
+          <Ventaja titulo="Comunicación" img="https://cdn-icons-png.flaticon.com/512/2838/2838133.png">
+            Acercamiento directo entre administrador y estudiante.
+          </Ventaja>
+        </div>
+      </section>
+
+      <footer className='text-center p-4 text-gray-400'>
+      <p>&copy; 2025 RoomiesU</p>
+      <p>Bucaramanga, Santander - Colombia</p>
+      </footer>
       
     </div>
   )
