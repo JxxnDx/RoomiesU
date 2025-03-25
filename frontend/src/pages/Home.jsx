e50b0bcf592a9311f02bcffc2f12ff5158597959
Juan Pallares <juan2220079@correo.uis.edu.co>
2025-03-24 22:19:56 -0500

    Segunda versión home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,19 +3,20 @@ import { Link } from "react-router-dom"
 import Ventaja from '../components/ventajas'
 
 export default function Home() {
+
   return (
     <div className="font-montserrat text-gray-900 m-0 p-0">
       <header className="flex flex-row justify-between bg-gray-900 text-white top-0 left-0 text-justify items-center p-4 w-full">
         <h1 className="text-4xl font-bold">RoomiesU</h1>
         <nav>
-          <ul className="flex gap-2 text-green-600 font-bold">
+          <ul className="flex gap-2 text-green-200 font-bold">
             <li>
-              <Link to="#" className="links-auth">
+              <Link to="/login" className="px-2 py-2 hover:text-white transition-all">
                 Iniciar Sesión
               </Link>
             </li>
             <li>
-              <Link to="#" className="links-auth">
+              <Link to="/register" className="px-2 py-2 hover:text-white transition-all">
                 Registrarse
               </Link>
             </li>
@@ -23,22 +24,15 @@ export default function Home() {
         </nav>
       </header>
 
-      <div className="relative flex items-center justify-center bg-white w-[80%] mx-auto my-10">
-        <div className="flex justify-center">
-          <img src="../layout_auth_roomiesu.webp" alt="habitacion" 
-               className="w-[70%] object-cover rounded-4xl shadow-2xl"/>
-        </div>
-        <article className="absolute top-[30%] transform -translate-y-1/2 bg-white/75 shadow-2xl rounded-2xl p-8 w-auto text-2xl text-center">
-          <h1 className="text-6xl font-bold">Bienvenido/a!</h1>
-          <p className="text-gray-700 m-8">
-            Encuentra la habitación perfecta para ti en RoomiesU. <br />
-            Un segundo hogar para tu vida universitaria.
-          </p>
-        </article> 
-      </div>
-      
+      <article className="text-5xl mx-auto my-0 text-center w-[60%] pt-20">
+        <h1 className="text-7xl font-bold">Bienvenido/a!</h1>
+        <p className="text-gray-700 m-8">
+          Encuentra la habitación perfecta para ti en RoomiesU. <br />
+          Un segundo hogar para tu vida universitaria.
+        </p>
+      </article>
 
-      <section className='p-10 border-2 border-green-600 rounded-2xl w-[60%] mx-auto my-20'> 
+      <section className='p-10 border-2 border-green-200 rounded-2xl w-[60%] mx-auto my-20'> 
         <h2 className="text-4xl font-bold text-center pb-8">¿Por qué RoomiesU?</h2>
         <div className="grid grid-cols-2 grid-rows-2 gap-8">
           <Ventaja titulo="Seguridad" img="../seguridad.png">
