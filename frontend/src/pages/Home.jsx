8bfb982b2aaab7108abd6592714c7576d83d4774
Juan Pallares <juan2220079@correo.uis.edu.co>
2025-03-24 22:24:56 -0500

    cambiando color borde y login

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -9,7 +9,7 @@ export default function Home() {
       <header className="flex flex-row justify-between bg-gray-900 text-white top-0 left-0 text-justify items-center p-4 w-full">
         <h1 className="text-4xl font-bold">RoomiesU</h1>
         <nav>
-          <ul className="flex gap-2 text-green-200 font-bold">
+          <ul className="flex gap-2 text-green-600 font-bold">
             <li>
               <Link to="/login" className="px-2 py-2 hover:text-white transition-all">
                 Iniciar Sesión
@@ -32,7 +32,7 @@ export default function Home() {
         </p>
       </article>
 
-      <section className='p-10 border-2 border-green-200 rounded-2xl w-[60%] mx-auto my-20'> 
+      <section className='p-10 border-2 border-green-600 rounded-2xl w-[60%] mx-auto my-20'> 
         <h2 className="text-4xl font-bold text-center pb-8">¿Por qué RoomiesU?</h2>
         <div className="grid grid-cols-2 grid-rows-2 gap-8">
           <Ventaja titulo="Seguridad" img="../seguridad.png">
