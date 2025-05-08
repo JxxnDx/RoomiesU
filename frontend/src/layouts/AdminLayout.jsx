4f9dfaf03a8ea87176510da07da5ab022f0dbef8
Juan Pallares <juan2220079@correo.uis.edu.co>
2025-05-08 12:57:13 -0500

    Iniciando admindashboard

--- frontend/src/layouts/AdminLayout.jsx
+++ frontend/src/layouts/AdminLayout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import SideBar from "../components/SideBar";
 import { useState } from "react";
-import NavbarAdmin from "../components/Navbar";
+import Navbar from "../components/Navbar";
 
 const AdminLayout = () => {
   const [sidebarToggle, setSidebarToggle] = useState(false);
@@ -11,7 +11,7 @@ const AdminLayout = () => {
       <SideBar sidebarToggle={sidebarToggle} setSidebarToggle={setSidebarToggle} />
       {/* El div principal ahora tiene margen izquierdo dinámico */}
       <div className={`transition-all duration-300 antialiased text-slate-500 dark:text-slate-400 bg-white dark:bg-slate-900 min-h-screen ${sidebarToggle ? "ml-64" : "ml-0"} `}>
-        <NavbarAdmin sidebarToggle={sidebarToggle} setSidebarToggle={setSidebarToggle} />
+        <Navbar sidebarToggle={sidebarToggle} setSidebarToggle={setSidebarToggle} />
         <Outlet />
       </div>
     </div>
